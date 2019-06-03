bbb79d7b33a4f591a186a836581dc0df2ac19637
Maxime Mouchet <max@maxmouchet.com>
2019-06-03 19:01:20 +0200

    Set version to 0.1 until final release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sncf-cli',
-    version='1.0',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
