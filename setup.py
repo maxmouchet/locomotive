aa79a6bd1c92830a74ed2775f4f077287a6f604b
Maxime Mouchet <max@maxmouchet.com>
2019-06-03 21:29:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='sncf-cli',
     version='0.1',
+    description='Search SNCF journeys from the CLI',
+    url='https://github.com/yafeunteun/sncf-cli',
+    author='Yann Feunteun, Maxime Mouchet',
+    license='MIT',
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
