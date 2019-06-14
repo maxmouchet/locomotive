b5621dcc40ebfd394831d8e03047d6475f3de58d
Maxime Mouchet <max@maxmouchet.com>
2019-06-14 19:21:54 +0200

    Package: make PEP 561 compatible

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        '': ['*.csv', '*.mustache'],
+        'locomotive': ['py.typed', '*.csv', '*.mustache'],
     },
     install_requires=[
         'attrs',
