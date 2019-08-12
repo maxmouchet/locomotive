6d9f26c28431e73596530fd9e2f6462a868ae3ff
Maxime Mouchet <max@maxmouchet.com>
2019-08-12 21:40:17 +0200

    CLI: Fix passengers

--- locomotive/stores.py
+++ locomotive/stores.py
@@ -45,7 +45,7 @@ class Passengers:
     def __passengers_from_json(cls, path: Path) -> List[Passenger]:
         passengers = []
         for obj in json.load(open(path)):
-            obj["birthday"] = dt.datetime.strptime(obj["birthday"], "%Y-%m-%d")
+            obj["birthday"] = dt.datetime.strptime(obj["birthday"], "%Y-%m-%d").date()
             passengers.append(Passenger(**obj))
         return passengers
 
