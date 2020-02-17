dcce388389450d9bb3227145de2086457a90e702
Maxime Mouchet <max@maxmouchet.com>
2020-02-17 20:37:05 +0100

    Remove stubs

--- stubs/dateparser.pyi
+++
@@ -1,4 +0,0 @@
-import datetime as dt
-from typing import Optional
-
-def parse(date_string: str, settings: dict = {}) -> Optional[dt.datetime]: ...
--- stubs/geopy/distance.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Tuple
-
-class Distance:
-    @property
-    def km(self) -> float: ...
-
-def distance(a: Tuple[float, float], b: Tuple[float, float]) -> Distance: ...
